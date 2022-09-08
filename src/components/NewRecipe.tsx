5526efe78f751668a6c4dacd299518b27cd2fa23
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2022-09-08 11:15:19 +0300

    added steps
    in the same way I added ingredients but this time added steps with sub steps inputs.

--- src/components/NewRecipe.tsx
+++ src/components/NewRecipe.tsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { copyJsxObj, recipeConsructor } from '../helpers/formHelpers';
 import IngredientInput from './IngredientInput';
+import StepSection from './StepSection';
 
 export default function NewRecipe() {
   const [ingredientsInputs, setIngredientsInputs] = useState<{
     [key: number]: JSX.Element;
   }>({});
+  const [stepsSections, setStepsSections] = useState<{
+    [key: number]: JSX.Element;
+  }>({});
+
   const addIngredient = () => {
     setIngredientsInputs((prev) => {
       const prevCopy = copyJsxObj(prev);
@@ -31,6 +36,31 @@ export default function NewRecipe() {
     });
   };
 
+  const addStepSection = () => {
+    setStepsSections((prev) => {
+      const prevCopy = copyJsxObj(prev);
+      let i =
+        Math.max(...Object.keys(prevCopy).map((numStr) => Number(numStr))) + 1;
+      if (i === -Infinity) i = 1;
+
+      prevCopy[i] = (
+        <StepSection
+          index={i}
+          removeStep={removeStep}
+          key={`stepSection #${i}`}
+        />
+      );
+      return prevCopy;
+    });
+  };
+  const removeStep = (i: number): void => {
+    setStepsSections((prev) => {
+      const prevCopy = copyJsxObj(prev);
+      delete prevCopy[i];
+      return prevCopy;
+    });
+  };
+
   useEffect(() => {
     setIngredientsInputs({
       1: (
@@ -39,6 +69,11 @@ export default function NewRecipe() {
           removeIngredient={removeIngredient}
           key={'ingredient #1'}
         />
+      ),
+    });
+    setStepsSections({
+      1: (
+        <StepSection index={1} removeStep={removeStep} key={'stepSection #1'} />
       ),
     });
   }, []);
@@ -58,6 +93,11 @@ export default function NewRecipe() {
         </button>
       </ul>
       <ol>
+        {Object.values(stepsSections)}
+        <button type='button' onClick={addStepSection}>
+          + new step
+        </button>
+      </ol>
     </form>
   );
 }
---
+++ src/components/StepSection.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import { copyJsxObj } from '../helpers/formHelpers';
+import StepInput from './StepInput';
+
+export default function StepSection({
+  index,
+  removeStep,
+}: {
+  index: number;
+  removeStep: (i: number) => void;
+}) {
+  const [stepsInputs, setStepsInputs] = useState<{
+    [key: number]: JSX.Element;
+  }>({});
+  const addStepSection = () => {
+    setStepsInputs((prev) => {
+      const prevCopy = copyJsxObj(prev);
+      let i =
+        Math.max(...Object.keys(prevCopy).map((numStr) => Number(numStr))) + 1;
+      if (i === -Infinity) i = 1;
+      prevCopy[i] = (
+        <StepInput
+          index={i}
+          removeStep={removeStepInput}
+          stepIndex={index}
+          key={`subStep #${i}`}
+        />
+      );
+      return prevCopy;
+    });
+  };
+  const removeStepInput = (i: number): void => {
+    setStepsInputs((prev) => {
+      const prevCopy = copyJsxObj(prev);
+      delete prevCopy[i];
+      return prevCopy;
+    });
+  };
+  useEffect(() => {
+    setStepsInputs({
+      1: (
+        <StepInput
+          index={1}
+          removeStep={removeStepInput}
+          stepIndex={index}
+          key={'subStep #1'}
+        />
+      ),
+    });
+  }, []);
+
+  return (
+    <li>
+      <div className='step-title'>
+        <h5>step title:</h5>
+        <input type='textarea' name={`step-name #${index}`} required />
+      </div>
+      <ol>{Object.values(stepsInputs)}</ol>
+      <button type='button' onClick={addStepSection}>
+        add sub step
+      </button>
+      <button
+        type='button'
+        onClick={() => {
+          removeStep(index);
+        }}
+      >
+        remove step
+      </button>
+    </li>
+  );
+}
